ddec1ca0e59a953ee2629d49f2b79e78bc56c21d
Het Pandya <55797177+thehetpandya@users.noreply.github.com>
2020-10-06 16:16:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='youtube_tts_data_generator',
-    version='0.1.2',
+    version='0.1.3',
     description='A python library that generates speech data with transcriptions by collecting data from YouTube.',
     long_description=long_description,
     long_description_content_type="text/markdown",
